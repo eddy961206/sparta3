cd3f30369ce0ba974124777282de237df3098cd1
HyungjunJeon <maru105kr@gmail.com>
2022-03-29 00:05:01 +0900

    로컬 프로젝트 최신화

--- app.py
+++ app.py
@@ -17,24 +17,15 @@ def main():
 def make():
     return render_template("make.html")
 
-
-@app.route("/upload", methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
-    ## file upload ##
-    img = request.files['image']
-
-    ## GridFs를 통해 파일을 분할하여 DB에 저장하게 된다
-    fs = gridfs.GridFS(db)
-    file_img_id = fs.put(img)
-
-    db.contents.insert_one({'img':file_img_id})
-
-    return jsonify({'msg': '저장에 성공했습니다.'})
-
-
-@app.route('/result')
-def result():
-    return render_template("result.html")
+    babyname = request.form['baby_name']
+    mothername = request.form['mother_name']
+    fathername = request.form['father_name']
+    birthyear = request.form['birth_year']
+    birthmonth = request.form['birth_month']
+    birthday = request.form['birth_day']
+    db.contents.insert_one({'babyName': babyname, 'motherName': mothername, 'fatherName': fathername, 'birthYear': birthyear, 'birthMonth': birthmonth, 'birthDay': birthday})
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
