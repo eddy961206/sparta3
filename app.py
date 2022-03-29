16416cbfec6045a14aab58d5bf05ad1a440e518f
gutner <refromto@naver.com>
2022-03-29 20:15:30 +0900

    사진 업로드 서버 업로드 테스트

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import gridfs
 app = Flask(__name__)
 
 from pymongo import MongoClient
-
 client = MongoClient('localhost', 27017)
-
 db = client.baby_photo
 
 @app.route('/')
@@ -17,12 +15,25 @@ def main():
 def make():
     return render_template("make.html")
 
-<<<<<<< HEAD
-@app.route('/upload', methods=['POST'])
-=======
+@app.route('/result')
+def result():
+    return render_template("result.html")
 
-@app.route("/upload", methods=['POST'])
->>>>>>> 류승택브랜치
+@app.route('/photo', methods=['POST'])
+def photo():
+    img = request.files['image']
+    fs = gridfs.GridFS(db)
+    fs.put(img, filename='photo')
+    ## file find ##
+    data = client.grid_file.fs.files.find_one({'filename': 'name'})
+
+    ## file download ##
+    photoID = data['_id']
+    outputdata = fs.get(photoID).read()
+    output = open('./images/' + 'back.jpeg', 'wb')
+    output.write(outputdata)
+
+@app.route('/upload', methods=['POST'])
 def upload():
     babyname = request.form['baby_name']
     mothername = request.form['mother_name']
@@ -30,31 +41,8 @@ def upload():
     birthyear = request.form['birth_year']
     birthmonth = request.form['birth_month']
     birthday = request.form['birth_day']
-<<<<<<< HEAD
     db.contents.insert_one({'babyName': babyname, 'motherName': mothername, 'fatherName': fathername, 'birthYear': birthyear, 'birthMonth': birthmonth, 'birthDay': birthday})
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
-=======
-    img = request.files['image']
 
-    fs = gridfs.GridFS(db)
-    file_img_id = fs.put(img)
-
-    db.contents.insert_one({
-        'babyName': babyname,
-        'motherName': mothername,
-        'fatherName': fathername,
-        'birthYear': birthyear,
-        'birthMonth': birthmonth,
-        'birthDay': birthday,
-        'img': file_img_id})
-
-
-@app.route('/result')
-def result():
-    return render_template("result.html")
-
-if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True)
->>>>>>> 류승택브랜치
