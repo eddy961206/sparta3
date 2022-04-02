54a1a74432c211e80f25ba698f3e4d514ada205f
gutner <refromto@naver.com>
2022-04-02 17:36:38 +0900

    _id값으로 폴더 생성후 해당 폴더에 사진 넣는 기능 구현

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+import os
 
+from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -28,6 +29,12 @@ def result():
 @app.route("/make_photo", methods=['get', 'POST'])
 def image_save():
     if request.method == 'POST':
+        newdata = list(db.contents.find({}))[-1]
+        _id = newdata['_id']
+        current_path = './static/'
+        if not os.path.isdir(current_path + "/" + str(_id)): os.makedirs(current_path + "/" + str(_id))
+        newfolder = current_path + "/" + str(_id) + "/"
+
         photo_present = request.files['photopresent']
         photo_family = request.files['photofamily']
         photo_1d = request.files['photo1d']
@@ -43,20 +50,20 @@ def image_save():
         photo_11mth = request.files['photo11mth']
         photo_12mth = request.files['photo12mth']
 
-        photo_present.save('./static/' + secure_filename(photo_present.filename))
-        photo_family.save('./static/' + secure_filename(photo_family.filename))
-        photo_1d.save('./static/' + secure_filename(photo_1d.filename))
-        photo_50d.save('./static/' + secure_filename(photo_50d.filename))
-        photo_100d.save('./static/' + secure_filename(photo_100d.filename))
-        photo_4mth.save('./static/' + secure_filename(photo_4mth.filename))
-        photo_5mth.save('./static/' + secure_filename(photo_5mth.filename))
-        photo_6mth.save('./static/' + secure_filename(photo_6mth.filename))
-        photo_7mth.save('./static/' + secure_filename(photo_7mth.filename))
-        photo_8mth.save('./static/' + secure_filename(photo_8mth.filename))
-        photo_9mth.save('./static/' + secure_filename(photo_9mth.filename))
-        photo_10mth.save('./static/' + secure_filename(photo_10mth.filename))
-        photo_11mth.save('./static/' + secure_filename(photo_11mth.filename))
-        photo_12mth.save('./static/' + secure_filename(photo_12mth.filename))
+        photo_present.save(newfolder + secure_filename(photo_present.filename))
+        photo_family.save(newfolder + secure_filename(photo_family.filename))
+        photo_1d.save(newfolder + secure_filename(photo_1d.filename))
+        photo_50d.save(newfolder + secure_filename(photo_50d.filename))
+        photo_100d.save(newfolder + secure_filename(photo_100d.filename))
+        photo_4mth.save(newfolder + secure_filename(photo_4mth.filename))
+        photo_5mth.save(newfolder + secure_filename(photo_5mth.filename))
+        photo_6mth.save(newfolder + secure_filename(photo_6mth.filename))
+        photo_7mth.save(newfolder + secure_filename(photo_7mth.filename))
+        photo_8mth.save(newfolder + secure_filename(photo_8mth.filename))
+        photo_9mth.save(newfolder + secure_filename(photo_9mth.filename))
+        photo_10mth.save(newfolder + secure_filename(photo_10mth.filename))
+        photo_11mth.save(newfolder + secure_filename(photo_11mth.filename))
+        photo_12mth.save(newfolder + secure_filename(photo_12mth.filename))
         return 'uploads 디렉토리 -> 파일 업로드 성공!'
 
 
@@ -80,6 +87,11 @@ def upload():
 def read_info():
     info = list(db.contents.find({}, {'_id': False}))
     return jsonify({'all_info': info})
+
+
+path = 'c:/1/2/3/4/5'
+if not os.path.isdir(path): os.makedirs(path)
+
 
 
 if __name__ == '__main__':
