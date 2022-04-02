0620f0028b4fd5af35dad01e000d9676c76d2698
gutner <refromto@naver.com>
2022-04-02 23:58:51 +0900

    파일이름 적용

--- app.py
+++ app.py
@@ -50,20 +50,20 @@ def image_save():
         photo_11mth = request.files['photo11mth']
         photo_12mth = request.files['photo12mth']
 
-        photo_present.save(newfolder + secure_filename(photo_present.filename))
-        photo_family.save(newfolder + secure_filename(photo_family.filename))
-        photo_1d.save(newfolder + secure_filename(photo_1d.filename))
-        photo_50d.save(newfolder + secure_filename(photo_50d.filename))
-        photo_100d.save(newfolder + secure_filename(photo_100d.filename))
-        photo_4mth.save(newfolder + secure_filename(photo_4mth.filename))
-        photo_5mth.save(newfolder + secure_filename(photo_5mth.filename))
-        photo_6mth.save(newfolder + secure_filename(photo_6mth.filename))
-        photo_7mth.save(newfolder + secure_filename(photo_7mth.filename))
-        photo_8mth.save(newfolder + secure_filename(photo_8mth.filename))
-        photo_9mth.save(newfolder + secure_filename(photo_9mth.filename))
-        photo_10mth.save(newfolder + secure_filename(photo_10mth.filename))
-        photo_11mth.save(newfolder + secure_filename(photo_11mth.filename))
-        photo_12mth.save(newfolder + secure_filename(photo_12mth.filename))
+        photo_present.save(newfolder + 'photo_present' + secure_filename(photo_present.filename))
+        photo_family.save(newfolder + 'photo_family' + secure_filename(photo_family.filename))
+        photo_1d.save(newfolder + 'photo_1d' + secure_filename(photo_1d.filename))
+        photo_50d.save(newfolder + 'photo_50d' + secure_filename(photo_50d.filename))
+        photo_100d.save(newfolder + 'photo_100d' + secure_filename(photo_100d.filename))
+        photo_4mth.save(newfolder + 'photo_4mth' + secure_filename(photo_4mth.filename))
+        photo_5mth.save(newfolder + 'photo_5mth' + secure_filename(photo_5mth.filename))
+        photo_6mth.save(newfolder + 'photo_6mth' + secure_filename(photo_6mth.filename))
+        photo_7mth.save(newfolder + 'photo_7mth' + secure_filename(photo_7mth.filename))
+        photo_8mth.save(newfolder + 'photo_8mth' + secure_filename(photo_8mth.filename))
+        photo_9mth.save(newfolder + 'photo_9mth' + secure_filename(photo_9mth.filename))
+        photo_10mth.save(newfolder + 'photo_10mth' + secure_filename(photo_10mth.filename))
+        photo_11mth.save(newfolder + 'photo_11mth' + secure_filename(photo_11mth.filename))
+        photo_12mth.save(newfolder + 'photo_12mth' + secure_filename(photo_12mth.filename))
         return 'uploads 디렉토리 -> 파일 업로드 성공!'
 
 
