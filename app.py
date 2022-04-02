a53c562c91e3ece1589656be35ca3a570dcd683d
woodybuzz02 <ajdanddl6321@gmail.com>
2022-04-02 23:54:20 +0900

    0402기준 최신

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 
 from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
@@ -5,24 +6,37 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
 db = client.baby_photo
+
 
 @app.route('/')
 def main():
     return render_template("main.html")
 
+
 @app.route('/make')
 def make():
     return render_template("make.html")
+
 
 @app.route('/result')
 def result():
     return render_template("result.html")
 
+
 @app.route("/make_photo", methods=['get', 'POST'])
 def image_save():
     if request.method == 'POST':
+        newdata = list(db.contents.find({}))[-1]
+        _id = newdata['_id']
+        current_path = './static/'
+        if not os.path.isdir(current_path + "/" + str(_id)): os.makedirs(current_path + "/" + str(_id))
+        newfolder = current_path + "/" + str(_id) + "/"
+
+        photo_present = request.files['photopresent']
+        photo_family = request.files['photofamily']
         photo_1d = request.files['photo1d']
         photo_50d = request.files['photo50d']
         photo_100d = request.files['photo100d']
@@ -35,20 +49,22 @@ def image_save():
         photo_10mth = request.files['photo10mth']
         photo_11mth = request.files['photo11mth']
         photo_12mth = request.files['photo12mth']
-        photo_1d.save('./static/' + secure_filename(photo_1d.filename))
-        photo_50d.save('./static/' + secure_filename(photo_50d.filename))
-        photo_100d.save('./static/' + secure_filename(photo_100d.filename))
-        photo_4mth.save('./static/' + secure_filename(photo_4mth.filename))
-        photo_5mth.save('./static/' + secure_filename(photo_5mth.filename))
-        photo_6mth.save('./static/' + secure_filename(photo_6mth.filename))
-        photo_7mth.save('./static/' + secure_filename(photo_7mth.filename))
-        photo_8mth.save('./static/' + secure_filename(photo_8mth.filename))
-        photo_9mth.save('./static/' + secure_filename(photo_9mth.filename))
-        photo_10mth.save('./static/' + secure_filename(photo_10mth.filename))
-        photo_11mth.save('./static/' + secure_filename(photo_11mth.filename))
-        photo_12mth.save('./static/' + secure_filename(photo_12mth.filename))
-        return 'uploads 디렉토리 -> 파일 업로드 성공!'
+        photo_present.save(newfolder + 'photo_present' + secure_filename(photo_present.filename))
+        photo_family.save(newfolder + 'photo_family' + secure_filename(photo_family.filename))
 
+        photo_1d.save(newfolder + secure_filename(photo_1d.filename))
+        photo_50d.save(newfolder + secure_filename(photo_50d.filename))
+        photo_100d.save(newfolder + secure_filename(photo_100d.filename))
+        photo_4mth.save(newfolder + secure_filename(photo_4mth.filename))
+        photo_5mth.save(newfolder + secure_filename(photo_5mth.filename))
+        photo_6mth.save(newfolder + secure_filename(photo_6mth.filename))
+        photo_7mth.save(newfolder + secure_filename(photo_7mth.filename))
+        photo_8mth.save(newfolder + secure_filename(photo_8mth.filename))
+        photo_9mth.save(newfolder + secure_filename(photo_9mth.filename))
+        photo_10mth.save(newfolder + secure_filename(photo_10mth.filename))
+        photo_11mth.save(newfolder + secure_filename(photo_11mth.filename))
+        photo_12mth.save(newfolder + secure_filename(photo_12mth.filename))
+        return 'uploads 디렉토리 -> 파일 업로드 성공!'
 
 
 @app.route('/upload', methods=['POST'])
@@ -59,7 +75,20 @@ def upload():
     birthyear = request.form['birth_year']
     birthmonth = request.form['birth_month']
     birthday = request.form['birth_day']
-    db.contents.insert_one({'babyName': babyname, 'motherName': mothername, 'fatherName': fathername, 'birthYear': birthyear, 'birthMonth': birthmonth, 'birthDay': birthday})
+    birthhour = request.form['birth_hour']
+    birthminute = request.form['birth_minute']
+
+    db.contents.insert_one(
+        {'babyName': babyname, 'motherName': mothername, 'fatherName': fathername, 'birthYear': birthyear,
+         'birthMonth': birthmonth, 'birthDay': birthday,'birthHour':birthhour, 'birthMinute': birthminute })
+
+
+@app.route('/info', methods=['GET'])
+def read_info():
+    info = list(db.contents.find({}, {'_id': False}))
+    return jsonify({'all_info': info})
+
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
