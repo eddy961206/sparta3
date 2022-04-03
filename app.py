00e86500c6111d9c88a1d68224ee813fa7d8498a
ryu <skykum2018@gmail.com>
2022-04-03 23:18:34 +0900

    4.3 마스터에

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
-import codecs
+import os
 
 from flask import Flask, render_template, request, jsonify
-import gridfs
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
 db = client.baby_photo
-fs = gridfs.GridFS(db)
+
 
 @app.route('/')
 def main():
     return render_template("main.html")
 
+
 @app.route('/make')
 def make():
     return render_template("make.html")
+
 
 @app.route('/result')
 def result():
     return render_template("result.html")
 
-@app.route("/make_photo", methods=['get', 'POST'])
+
+@app.route("/make_photo", methods=['GET', 'POST'])
 def image_save():
+    # id값 받아오는 중복 부분 중복 제거
+    newdata = list(db.contents.find({}))[-1]
+    _id = newdata['_id']
     if request.method == 'POST':
+        current_path = './static/'
+        if not os.path.isdir(current_path + "/" + str(_id)): os.makedirs(current_path + "/" + str(_id))
+        newfolder = current_path + "/" + str(_id) + "/"
+
+        photo_present = request.files['photopresent']
+        photo_family = request.files['photofamily']
         photo_1d = request.files['photo1d']
         photo_50d = request.files['photo50d']
         photo_100d = request.files['photo100d']
@@ -38,21 +50,37 @@ def image_save():
         photo_10mth = request.files['photo10mth']
         photo_11mth = request.files['photo11mth']
         photo_12mth = request.files['photo12mth']
-        photo_1d.save('./static/' + secure_filename(photo_1d.filename))
-        photo_50d.save('./static/' + secure_filename(photo_50d.filename))
-        photo_100d.save('./static/' + secure_filename(photo_100d.filename))
-        photo_4mth.save('./static/' + secure_filename(photo_4mth.filename))
-        photo_5mth.save('./static/' + secure_filename(photo_5mth.filename))
-        photo_6mth.save('./static/' + secure_filename(photo_6mth.filename))
-        photo_7mth.save('./static/' + secure_filename(photo_7mth.filename))
-        photo_8mth.save('./static/' + secure_filename(photo_8mth.filename))
-        photo_9mth.save('./static/' + secure_filename(photo_9mth.filename))
-        photo_10mth.save('./static/' + secure_filename(photo_10mth.filename))
-        photo_11mth.save('./static/' + secure_filename(photo_11mth.filename))
-        photo_12mth.save('./static/' + secure_filename(photo_12mth.filename))
+        photo_present.save(newfolder + 'a_photo_present' + secure_filename(photo_present.filename))
+        photo_family.save(newfolder + 'b_photo_family' + secure_filename(photo_family.filename))
+        photo_1d.save(newfolder + 'c_photo_1d' + secure_filename(photo_1d.filename))
+        photo_50d.save(newfolder + 'd_photo_50d' + secure_filename(photo_50d.filename))
+        photo_100d.save(newfolder + 'e_photo_100d' + secure_filename(photo_100d.filename))
+        photo_4mth.save(newfolder + 'f_photo_4mth' + secure_filename(photo_4mth.filename))
+        photo_5mth.save(newfolder + 'g_photo_5mth' + secure_filename(photo_5mth.filename))
+        photo_6mth.save(newfolder + 'h_photo_6mth' + secure_filename(photo_6mth.filename))
+        photo_7mth.save(newfolder + 'i_photo_7mth' + secure_filename(photo_7mth.filename))
+        photo_8mth.save(newfolder + 'j_photo_8mth' + secure_filename(photo_8mth.filename))
+        photo_9mth.save(newfolder + 'k_photo_9mth' + secure_filename(photo_9mth.filename))
+        photo_10mth.save(newfolder + 'l_photo_10mth' + secure_filename(photo_10mth.filename))
+        photo_11mth.save(newfolder + 'm_photo_11mth' + secure_filename(photo_11mth.filename))
+        photo_12mth.save(newfolder + 'n_photo_12mth' + secure_filename(photo_12mth.filename))
+        photo_1d.save(newfolder + secure_filename(photo_1d.filename))
+        photo_50d.save(newfolder + secure_filename(photo_50d.filename))
+        photo_100d.save(newfolder + secure_filename(photo_100d.filename))
+        photo_4mth.save(newfolder + secure_filename(photo_4mth.filename))
+        photo_5mth.save(newfolder + secure_filename(photo_5mth.filename))
+        photo_6mth.save(newfolder + secure_filename(photo_6mth.filename))
+        photo_7mth.save(newfolder + secure_filename(photo_7mth.filename))
+        photo_8mth.save(newfolder + secure_filename(photo_8mth.filename))
+        photo_9mth.save(newfolder + secure_filename(photo_9mth.filename))
+        photo_10mth.save(newfolder + secure_filename(photo_10mth.filename))
+        photo_11mth.save(newfolder + secure_filename(photo_11mth.filename))
+        photo_12mth.save(newfolder + secure_filename(photo_12mth.filename))
         return 'uploads 디렉토리 -> 파일 업로드 성공!'
 
-
+    else: # if request.method == 'GET':
+        photos = os.listdir('./static/' + str(_id) + '/') # static/id로 만든 폴더 안의 파일 리스트를 photos 변수에 저장
+        return jsonify({'all_photos': photos, 'id': str(_id)}) # photos 변수에 담긴 파일리스트와 id값을 함께 json 형태로 전달
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -62,7 +90,20 @@ def upload():
     birthyear = request.form['birth_year']
     birthmonth = request.form['birth_month']
     birthday = request.form['birth_day']
-    db.contents.insert_one({'babyName': babyname, 'motherName': mothername, 'fatherName': fathername, 'birthYear': birthyear, 'birthMonth': birthmonth, 'birthDay': birthday})
+    birthhour = request.form['birth_hour']
+    birthminute = request.form['birth_minute']
+
+    db.contents.insert_one(
+        {'babyName': babyname, 'motherName': mothername, 'fatherName': fathername, 'birthYear': birthyear,
+         'birthMonth': birthmonth, 'birthDay': birthday,'birthHour':birthhour, 'birthMinute': birthminute })
+
+
+@app.route('/info', methods=['GET'])
+def read_info():
+    info = list(db.contents.find({}, {'_id': False}))
+    return jsonify({'all_info': info})
+
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
